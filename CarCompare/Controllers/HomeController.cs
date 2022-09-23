6cfe4cb9c24ea9aba1ab92ebb4434402617809c2
ChristofferPerssons <chripe@student.chalmers.se>
2022-09-23 11:26:35 +0200

    Manually merging Tor Branch into main

--- CarCompare/Controllers/HomeController.cs
+++ CarCompare/Controllers/HomeController.cs
@@ -32,12 +32,6 @@ namespace CarCompare.Controllers
 
             return View();
         }
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         public ActionResult Error404()
         {
