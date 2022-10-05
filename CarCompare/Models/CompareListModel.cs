e3c24ba2e77f035917629018e4c1f63d0f0bc49d
Tor <tor88fg@gmail.com>
2022-10-05 17:27:08 +0200

    Filtering done! Sorting is almost there

--- CarCompare/Models/CompareListModel.cs
+++ CarCompare/Models/CompareListModel.cs
@@ -11,17 +11,37 @@ namespace CarCompare.Models
     //Vehicle model and the VehicleList model are in separate classes.
     public class CompareListModel
     {
-        //Private variables
-        private static VehicleList VehicleList;
-        private List<Vehicle> CurrentList;
+        //--Private variables--
 
-        //Constructor
+        //VehicleList model
+        private static VehicleList VehicleList;
+
+        //Lists
+        private List<Vehicle> SortedList, FilteredList;
+
+        //Filter variables
+        public Boolean showElectric, showHybrid;
+        public float accelerationMin, accelerationMax, yearMin, yearMax, rangeMin, rangeMax;
+        public List<String> specifiedBrands = new List<string>();
+        public List<String> specifiedSeats = new List<string>();
+
+
+
+        //--Constructor--
         public CompareListModel()
         {
-            //Generates VehicleList
+            //Generates VehicleList model
             VehicleList = new VehicleList();
-            //Creates CurrentList and sorts it by Co2 emission
-            CurrentList = Sort(VehicleList.Get(), "co2");
+
+            //Initializes SortedList and sorts it by Co2 emission
+            SortedList = VehicleList.Get();
+            Sort("co2", true);
+
+            //Sets the default filter variables
+            setDefaultFilter();
+
+            //Initializes FilteredList
+            Filter();
         }
 
         //--PUBLIC METHODS--
@@ -29,80 +49,142 @@ namespace CarCompare.Models
         //Get the VehicleArray
         public Vehicle[] GetArray()
         {
-            return CurrentList.ToArray();
+            return FilteredList.ToArray();
         }
 
-        //Filter current array (!!TODO!! - filter on other/multiple variables) 
+        //Filter current list after all modified filter variables
         public void Filter()
         {
             List<Vehicle> filteredList = new List<Vehicle>();
-            float compVar;
 
-            foreach (Vehicle vehicle in CurrentList)
+            //Loops through all vehicles
+            foreach (Vehicle vehicle in SortedList)
             {
-                compVar = 0;
-                if (!vehicle.GetModification("acceleration").Equals("Not Found"))
+
+                //Filters depending on ShowElectric/showHybrid variable
+                if(!showElectric && vehicle.isAllElectric()) { continue; }
+                if(!showHybrid && !vehicle.isAllElectric()) { continue; }
+
+                //Filters depending on brand variables if filtering by brands is enabled
+                if (brandsSpecified())
                 {
-                    compVar = float.Parse(vehicle.GetModification("acceleration"));
+                    Boolean isBrand = false;
+                    foreach(String brand in specifiedBrands)
+                    {
+                        if(vehicle.Brand.Equals(brand)) { 
+                            isBrand = true;
+                            break;
+                        }
+                    }
+                    if(!isBrand) { continue; }
                 }
 
-                if (compVar >= 10)
+                //Filters depending on seats variables if filtering by seats is enabled
+                if (seatsSpecified())
                 {
-                    filteredList.Add(vehicle);
+                    Boolean isSeatNr = false;
+                    foreach (String nrOfSeats in specifiedSeats)
+                    {
+                        if (vehicle.GetModification("places").Contains(nrOfSeats)) { 
+                            isSeatNr = true;
+                            break;
+                        }
+                    }
+                    if (!isSeatNr) { continue; }
                 }
+
+                //Filters depending on acceleration variables if filtering by acceleration is enabled
+                if (accelerationModified())
+                {
+                    float comp = vehicle.GetComp("acceleration");
+                    if(comp == -1) { continue; }
+                    if (accelerationMin != -1 && comp < accelerationMin) { continue; }
+                    if (accelerationMax != -1 && comp > accelerationMax) { continue; }
+                }
+
+                //Filters depending on year variables if filtering by year is enabled
+                if (yearModified())
+                {
+                    float comp = vehicle.GetComp("yearstart");
+                    if (comp == -1) { continue; }
+                    if (yearMin != -1 && comp < yearMin) { continue; }
+                    if (yearMax != -1 && comp > yearMax) { continue; }
+                }
+
+                //Filters depending on range variables if filtering by range is enabled
+                if (rangeModified())
+                {
+                    float comp = vehicle.GetComp("allElectricRange");
+                    if (comp == -1) { continue; }
+                    if (rangeMin != -1 && comp < rangeMin) { continue; }
+                    if (rangeMax != -1 && comp > rangeMax) { continue; }
+                }
+
+                //Adds vehicles that passed all filter tests to list
+                filteredList.Add(vehicle);
             }
 
-            CurrentList = filteredList;
+            //Sets Filtered list to newly created filteredList
+            FilteredList = filteredList;
         }
 
-        //Resets
+        //Resets filter variables sets FilteredList to full, sorted List.
         public void ResetFilter()
         {
-            CurrentList = Sort(VehicleList.Get(), "co2");
+            setDefaultFilter();
+            Filter();
         }
 
 
         //--PRIVATE METHODS--
 
-        //Sorts list by Co2 emission (!!TODO!! - Re-sort List depending on variable)
-        public List<Vehicle> Sort(List<Vehicle> List, string Modification)
+        
+
+        //Sorts list by Modification variabla. Ascending/Descending set through 'Ascending' boolean parameter.
+        public void Sort(String Modification, Boolean Ascending)
         {
-            if(Modification == "co2")
+            if (Modification == "co2")
             {
-                return List.OrderBy(
-                vehicle => {
-                    string value = vehicle.GetModification("co2");
-                    if (value == "Not Found" && vehicle.GetModification("fuelConsumptionCombined") == "Not Found")
-                    {
-                        return "0";
-                    }
-                    return value;
-                }
-            ).ToList();
-            }
-            else
+                if (Ascending) { this.SortedList = this.SortedList.OrderBy(vehicle => vehicle.GetModification("co2")).ToList(); }
+                else { this.SortedList = this.SortedList.OrderByDescending(vehicle => vehicle.GetModification("co2")).ToList(); }
+            } else
             {
-                return List.OrderBy(
-                vehicle => {
-                    string value = vehicle.GetModification(Modification);
-                    if (value == "Not Found")
-                    {
-                        return "0";
-                    }
-                    return value;
+                if (Ascending)
+                {
+                    this.SortedList = this.SortedList.OrderBy(vehicle => vehicle.GetModification("Modification")).ToList();
                 }
-                ).ThenBy(
-                    vehicle => {
-                        string value = vehicle.GetModification("co2");
-                        if (value == "Not Found" && vehicle.GetModification("fuelConsumptionCombined") == "Not Found")
-                        {
-                            return "0";
-                        }
-                        return value;
-                    }
-                ).ToList();
+                else { this.SortedList = this.SortedList.OrderByDescending(vehicle => vehicle.GetModification("Modification")).ThenBy(vehicle => vehicle.GetModification("co2")).ToList(); }
             }
 
+            Filter();
+        }
+
+        //Boolean functions determining if filter variables has been modified/specified
+        private Boolean brandsSpecified() { return specifiedBrands.Count != 0; }
+        private Boolean seatsSpecified() { return specifiedSeats.Count != 0; }
+        private Boolean accelerationModified() { return accelerationMax != -1 || accelerationMin != -1; }
+        private Boolean yearModified() { return yearMax != -1 || yearMin != -1; }
+        private Boolean rangeModified() { return rangeMax != -1 || rangeMin != -1; }
+
+        //Sets default filter variables
+        private void setDefaultFilter()
+        {
+            showElectric = false;
+            showHybrid = true;
+
+            accelerationMin = -1;
+            accelerationMax = -1;
+            yearMin = -1;
+            yearMax = -1;
+            rangeMin = -1;
+            rangeMax = -1;
+
+            specifiedBrands.Clear();
+            specifiedSeats.Clear();
+
+            //--TESTCODE--
+            //specifiedBrands.Add("Jeep");
+            //specifiedSeats.Add("7");
         }
     }
 
